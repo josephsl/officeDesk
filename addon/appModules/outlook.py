bb3cb36d708d6b39ab2d7f4d137697dcf16ca1d6
Joseph Lee <joseph.lee22590@gmail.com>
2022-01-09 17:58:22 -0800

    Outlook: remove Outlook app module altogether.
    Thanks to Outlook Extended add-on, there is no need for Office Desk to include Outlook app module, so remove the app module altogether.

--- addon/appModules/outlook.py
+++
@@ -1,7 +0,0 @@
-# A part of NonVisual Desktop Access (NVDA)
-# Copyright (C) 2006-2021 NV Access Limited, Yogesh Kumar, Manish Agrawal, Joseph Lee, Davy Kager,
-# Babbage B.V., Leonard de Ruijter
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
-
-from nvdaBuiltin.appModules.outlook import *

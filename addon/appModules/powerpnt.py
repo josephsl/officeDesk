94f5de72b0204a4b342e6fd034940e5019c9f364
Joseph Lee <joseph.lee22590@gmail.com>
2022-01-09 17:42:24 -0800

    Office apps: add alias app modules for Excel, Outlook, PowerPoint, and Word.
    Added alias app modules for Office applications - module contents to be filled by contributors.

---
+++ addon/appModules/powerpnt.py
@@ -0,0 +1,6 @@
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2012-2018 NV Access Limited
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+from nvdaBuiltin.appModules.powerpnt import *

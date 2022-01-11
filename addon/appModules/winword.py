4769ab088112a2cb20be239ad22f726e35cb0748
Joseph Lee <joseph.lee22590@gmail.com>
2022-01-10 16:35:50 -0800

    Word: remove Word app module in consideration for Word Access Enhancmenets add-on

--- addon/appModules/winword.py
+++
@@ -1,6 +0,0 @@
-# A part of NonVisual Desktop Access (NVDA)
-# This file is covered by the GNU General Public License.
-# See the file COPYING for more details.
-# Copyright (C) 2019-2020 NV Access Limited, Cyrille Bougot
-
-from nvdaBuiltin.appModules.winword import *

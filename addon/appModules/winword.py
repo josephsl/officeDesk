ff18a2f154a93adf7a8463cf6fd925c8cf67f181
Joseph Lee <joseph.lee22590@gmail.com>
2022-09-19 10:03:58 -0700

    Word: reuse code from NVDA Core's Word module as much as possible.
    Reuse code form NVDA Core's Word module as much as possible, reducing file size. In particular, track changes toggle command is removed from the add-on version.

--- addon/appModules/winword.py
+++ addon/appModules/winword.py
@@ -9,18 +9,14 @@ Word and Outlook share a lot of code and components. This app module gathers the
 Microsoft Word only.
 """
 
-import appModuleHandler
-from scriptHandler import script
-import ui
+from nvdaBuiltin.appModules.winword import AppModule, WinwordWordDocument
 import api
 from NVDAObjects.behaviors import Dialog
 from NVDAObjects.IAccessible.winword import WordDocument as IAccessibleWordDocument
 from NVDAObjects.UIA.wordDocument import WordDocument as UIAWordDocument
-from NVDAObjects.window.winword import WordDocument
-from NVDAObjects.UIA import UIA
 
 
-class AppModule(appModuleHandler.AppModule):
+class AppModule(AppModule):
 
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		if UIAWordDocument in clsList or IAccessibleWordDocument in clsList:
@@ -34,30 +30,12 @@ class AppModule(appModuleHandler.AppModule):
 		nextHandler()
 
 
-class WinwordWordDocument(WordDocument):
+class WinwordWordDocument(WinwordWordDocument):
 
 	def _get_name(self):
 		# Some places in Word have actual labels, not document title.
 		# NVDA Core issue 14156: notably, envelopes dialog edit fields do have usable labels.
 		name = super(WinwordWordDocument, self).name
-		if isinstance(self, UIA) and isinstance(api.getForegroundObject(), Dialog):
+		if isinstance(self, UIAWordDocument) and isinstance(api.getForegroundObject(), Dialog):
 			name = self.UIAElement.currentName
 		return name
-
-	@script(gesture="kb:control+shift+e")
-	def script_toggleChangeTracking(self, gesture):
-		if not self.WinwordDocumentObject:
-			# We cannot fetch the Word object model, so we therefore cannot report the status change.
-			# The object model may be unavailable because it's within Windows Defender Application Guard.
-			# In this case, just let the gesture through and don't report anything.
-			return gesture.send()
-		val = self._WaitForValueChangeForAction(
-			lambda: gesture.send(),
-			lambda: self.WinwordDocumentObject.TrackRevisions
-		)
-		if val:
-			# Translators: a message when toggling change tracking in Microsoft word
-			ui.message(_("Change tracking on"))
-		else:
-			# Translators: a message when toggling change tracking in Microsoft word
-			ui.message(_("Change tracking off"))
